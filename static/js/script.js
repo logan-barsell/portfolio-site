e3527f390125d9fba09cc3c8f7fef6d0f10e1c6e
Logan Barsell <loganjbars@gmail.com>
2022-07-06 00:13:33 -0700

    removed stats and fixed some bugs

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,4 @@
-$(window).on('load', function() {
+$(window).on('load', function () {
   $('.loader').fadeOut(700);
 
   $('.items').isotope({
@@ -11,36 +11,36 @@ $(window).on('load', function() {
   });
 });
 
-$(document).ready(function() {
+$(document).ready(function () {
 
   const navLinks = document.querySelectorAll('.nav-item');
   const menuToggle = document.getElementById('navbarNav');
   navLinks.forEach((l) => {
-      l.addEventListener('click', () => { 
-        const windoww = $(window).width();
-        if (windoww < 575) {
-          const bsCollapse = new bootstrap.Collapse(menuToggle);
-          bsCollapse.toggle();
-          console.log("toggled");
-        }
-      });
+    l.addEventListener('click', () => {
+      const windoww = $(window).width();
+      if (windoww < 575) {
+        const bsCollapse = new bootstrap.Collapse(menuToggle);
+        bsCollapse.toggle();
+        console.log("toggled");
+      }
+    });
   });
 
   $(document).click(function (event) {
     var clickover = $(event.target);
-    var $navbar = $(".navbar-collapse");               
+    var $navbar = $(".navbar-collapse");
     var _opened = $navbar.hasClass("show");
-    if (_opened === true && !clickover.hasClass("navbar-toggler")) {      
-        $navbar.collapse('hide');
+    if (_opened === true && !clickover.hasClass("navbar-toggler")) {
+      $navbar.collapse('hide');
     }
   });
 
   var myCarousel = document.querySelector('#myCarousel')
-  var carousel = new bootstrap.Carousel(myCarousel, {
+  new bootstrap.Carousel(myCarousel, {
     interval: 7000
-  })
+  });
 
-  var typed = new Typed('.typed', {
+  new Typed('.typed', {
     strings: ['Web Developer'],
     typeSpeed: 70,
     loop: false,
@@ -48,48 +48,47 @@ $(document).ready(function() {
     showCursor: false
   });
 
-  
 
-  var skillsTopOffset = $('.skillsSection').offset().top;
-  var statsTopOffset = $('.statsSection').offset().top;
-  var countUpFinished = false;
 
-  $(window).scroll(function() {
+  // var skillsTopOffset = $('.skillsSection').offset().top;
+  // var countUpFinished = false;
 
-    // if (window.pageYOffset > skillsTopOffset - $(window).height() + 200 ) {
-    //   $(function() {
-    //     $('.chart').easyPieChart({
-    //         easing: 'easeInOut',
-    //         barColor: '#fff',
-    //         trackColor: false,
-    //         scaleColor: false,
-    //         lineWidth: 4,
-    //         size: 152,
-    //         onStep: (from, to, percent) => {
-    //           $(this.el).find('.percent').text(Math.round(percent));
-    //         }
-    //     });
-    //   });
-    // }
+  // $(window).scroll(function () {
 
-    if (!countUpFinished && window.pageYOffset > statsTopOffset - $(window).height() + 200 ) {
-      $('.counter').each(function() {
-        var element = $(this);
-        var endVal = parseInt(element.text());
-    
-        element.countup(endVal);
-        countUpFinished = true;
-      });
-    }
+  //   // if (window.pageYOffset > skillsTopOffset - $(window).height() + 200 ) {
+  //   //   $(function() {
+  //   //     $('.chart').easyPieChart({
+  //   //         easing: 'easeInOut',
+  //   //         barColor: '#fff',
+  //   //         trackColor: false,
+  //   //         scaleColor: false,
+  //   //         lineWidth: 4,
+  //   //         size: 152,
+  //   //         onStep: (from, to, percent) => {
+  //   //           $(this.el).find('.percent').text(Math.round(percent));
+  //   //         }
+  //   //     });
+  //   //   });
+  //   // }
 
-  });
+  //   // if (!countUpFinished && window.pageYOffset > statsTopOffset - $(window).height() + 200) {
+  //   //   $('.counter').each(function () {
+  //   //     var element = $(this);
+  //   //     var endVal = parseInt(element.text());
+
+  //   //     element.countup(endVal);
+  //   //     countUpFinished = true;
+  //   //   });
+  //   // }
+
+  // });
 
   Fancybox.bind("[data-fancybox]", {
     // Your options go here
   });
 
 
-  $('#filters a').click(function() {
+  $('#filters a').click(function () {
     $('#filters .active').removeClass('active');
     $(this).addClass('active');
 
@@ -106,14 +105,14 @@ $(document).ready(function() {
     return false;
   });
 
-  $('.navigate a').click(function(e) {
+  $('.navigate a').click(function (e) {
     e.preventDefault();
     console.log(this);
     var targetElement = $(this).attr('href');
     var targetPosition = $(targetElement).offset().top;
-    
+
     $('html, body').animate({
-      scrollTop: targetPosition - 50
+      scrollTop: targetPosition - 500
     }, 400, 'linear')
   });
 
